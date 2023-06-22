fd8808bdb9142e1c5453210872195ba99d5d51bd
ArthurLima <arthurdeolima@gmail.com>
2023-06-21 22:08:05 -0300

    Building kaiser window

--- main.m
+++ main.m
@@ -3,3 +3,7 @@ clear;
 
 [audio, fs] = audioread('audio_corrompido 1.wav');
 plot_analysis(audio, fs);
+
+[kaiser_w, kaiser_n, kaizer_beta] = get_kaiser_window(6000, 7000, 0.01, fs);
+
+wvtool(kaiser_w);
