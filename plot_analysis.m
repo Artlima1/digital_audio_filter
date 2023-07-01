90746327d7cc778b0a02715edff9b70c0357a609
ArthurLima <arthurdeolima@gmail.com>
2023-06-30 21:52:10 -0300

    New plotting

--- plot_analysis.m
+++ plot_analysis.m
@@ -1,4 +1,4 @@
-function plot_analysis (x, y, kw, h, fs)
+function plot_analysis (x, y1, y2, kw, h, fs)
 
     T = 1/fs;
     L = length(x);
@@ -8,9 +8,13 @@ function plot_analysis (x, y, kw, h, fs)
     X_mod = abs(fftshift(X));
     X_mod_dB = mag2db(X_mod);
 
-    Y = fft(y);
-    Y_mod = abs(fftshift(Y));
-    Y_mod_dB = mag2db(Y_mod);
+    Y1 = fft(y1);
+    Y1_mod = abs(fftshift(Y1));
+    Y1_mod_dB = mag2db(Y1_mod);
+
+    Y2 = fft(y2);
+    Y2_mod = abs(fftshift(Y2));
+    Y2_mod_dB = mag2db(Y2_mod);
 
     H = fft(h, L);
     H_mod = abs(fftshift(H));
@@ -21,32 +25,42 @@ function plot_analysis (x, y, kw, h, fs)
     
     figure
     % Plot in time
-    subplot(2, 3, 1);
+    subplot(1, 3, 1);
     plot(t, x);
-    subplot(2, 3, 4);
-    plot(t, y);
+    subplot(1, 3, 2);
+    plot(t, y1);
+    subplot(1, 3, 3);
+    plot(t, y2);
 
+    figure
     % Plot in frequency
-    subplot(2, 3, 2);
+    subplot(1, 3, 1);
     plot(f, X_mod);
-    subplot(2, 3, 5);
-    plot(f, Y_mod);
+    subplot(1, 3, 2);
+    plot(f, Y1_mod);
+    subplot(1, 3, 3);
+    plot(f, Y2_mod);
 
+    figure
     % Plot in frequency (dB)
-    subplot(2, 3, 3);
+    subplot(1, 3, 1);
     plot(f, X_mod_dB);
-    subplot(2, 3, 6);
-    plot(f, Y_mod_dB);
+    subplot(1, 3, 2);
+    plot(f, Y1_mod_dB);
+    subplot(1, 3, 3);
+    plot(f, Y2_mod_dB);
 
     figure
     % Plot the spectrograms
-    subplot(1, 2, 1);
+    subplot(1, 3, 1);
     spectrogram(x, fs, 'yaxis');
-    subplot(1, 2, 2);
-    spectrogram(y, fs, 'yaxis');
+    subplot(1, 3, 2);
+    spectrogram(y1, fs, 'yaxis');
+    subplot(1, 3, 2);
+    spectrogram(y2, fs, 'yaxis');
 
     figure
-    % Plot the spectrograms
+    % Plot h[n] charts
     subplot(2, 2, 1);
     plot((1:length(kw)), kw);
     subplot(2, 2, 2);
